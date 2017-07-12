5c7b96a380f664c37fa1a15d8139fa398df4e98f
Tiperciuc Romeo <tiperciucromeo@gmail.com>
2017-07-12 19:01:24 +0300

    da

---
+++ UndeNeDam/app/src/main/java/com/romeotutorial/undenedam/ListaPartiiDeschis.java
@@ -0,0 +1,13 @@
+package com.romeotutorial.undenedam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListaPartiiDeschis extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_partii_deschis);
+    }
+}
